f28777e5b9f8fb5d9a63c009268ddde5350ecec2
Crowdbotics <engineering@crowdbotics.com>
2023-04-18 06:57:41 +0000

    Update store.

--- store/datamodeldevAPI/api.js
+++ store/datamodeldevAPI/api.js
@@ -9,6 +9,12 @@ function api_v1_login_create(payload) {
 function api_v1_signup_create(payload) {
   return datamodeldevAPI.post(`/api/v1/signup/`, payload.data)
 }
+function api_v1_test12_list(payload) {
+  return datamodeldevAPI.get(`/api/v1/test12/`)
+}
+function api_v1_test12_create(payload) {
+  return datamodeldevAPI.post(`/api/v1/test12/`, payload.data)
+}
 function rest_auth_user_retrieve(payload) {
   return datamodeldevAPI.get(`/rest-auth/user/`)
 }
@@ -31,6 +37,18 @@ function rest_auth_logout_retrieve(payload) {
 }
 function rest_auth_logout_create(payload) {
   return datamodeldevAPI.post(`/rest-auth/logout/`)
+}
+function api_v1_test12_retrieve(payload) {
+  return datamodeldevAPI.get(`/api/v1/test12/${payload.id}/`)
+}
+function api_v1_test12_update(payload) {
+  return datamodeldevAPI.put(`/api/v1/test12/${payload.id}/`, payload.data)
+}
+function api_v1_test12_partial_update(payload) {
+  return datamodeldevAPI.patch(`/api/v1/test12/${payload.id}/`, payload.data)
+}
+function api_v1_test12_destroy(payload) {
+  return datamodeldevAPI.delete(`/api/v1/test12/${payload.id}/`)
 }
 function rest_auth_registration_create(payload) {
   return datamodeldevAPI.post(`/rest-auth/registration/`, payload.data)
@@ -80,6 +98,8 @@ function rest_auth_registration_verify_email_create(payload) {
 export const apiService = {
   api_v1_login_create,
   api_v1_signup_create,
+  api_v1_test12_list,
+  api_v1_test12_create,
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update,
@@ -87,6 +107,10 @@ export const apiService = {
   rest_auth_login_create,
   rest_auth_logout_retrieve,
   rest_auth_logout_create,
+  api_v1_test12_retrieve,
+  api_v1_test12_update,
+  api_v1_test12_partial_update,
+  api_v1_test12_destroy,
   rest_auth_registration_create,
   modules_articles_article_list,
   modules_articles_article_create,
