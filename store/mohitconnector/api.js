a50c95cc984d1e165e944524e4ac2a1bb39f84e1
Crowdbotics <engineering@crowdbotics.com>
2023-04-18 09:48:10 +0000

    Update store.

--- store/mohitconnector/api.js
+++ store/mohitconnector/api.js
@@ -4,14 +4,20 @@ import {
   MOHIT_CONNECTOR_PASSWORD
 } from "react-native-dotenv"
 const mohitconnector = axios.create({
-  baseURL: "https://youtube.com",
+  baseURL: "https://test.com",
   auth: {
     username: MOHIT_CONNECTOR_USERNAME,
     password: MOHIT_CONNECTOR_PASSWORD
   },
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
+function mohitconnector_get__read(payload) {
+  return mohitconnector.get(`/`)
+}
 function mohitconnector_get_testing_read(payload) {
   return mohitconnector.get(`/testing`)
 }
-export const apiService = { mohitconnector_get_testing_read }
+export const apiService = {
+  mohitconnector_get__read,
+  mohitconnector_get_testing_read
+}
